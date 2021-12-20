5aec81cd3c9052d62f9f3ad1967c4b6c80fd4108
ganbari <steinway.nathan@gmail.com>
2021-12-19 20:27:04 -0600

    reengineered the process by moving the prompts and confirm windows into the generatePassword function and reformatting them

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
-// Assignment code here
+// Arrays of Character Choices
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 
 // Get references to the #generate element
@@ -18,96 +22,65 @@ function writePassword() {
 
 var generatePassword = function() {
 
-  passLength();
-  passSpCharacter();
-  passUpperCase();
-  passLowerCase();
-
-};
+  var passLengthPrompt = window.prompt("How many characters?");
+  var passLowerCaseConfirm = window.confirm("would you like your password to have lower case?");
+  var passUpperCaseConfirm = window.confirm("Would you like your password to have upper case?");
+  var passSpCharacterConfirm = window.confirm("Would you like your password to have special characters?");
 
 
-// BELOW ARE ALL PROMPT FUNCTIONS REFERRED TO IN generatePassword.
+  if (passLengthPrompt >= 8 && passLengthPrompt <= 128) {
 
-// passLength contains a prompt. 
-// --It checks for user input and determines if it is greater than or equal to 8 AND less than or equal to 128. 
-// --If user input is not within the specified range, it informs them of their mistake and loops back for them to try again.
-// The other three: passSpCharacter, passUpperCase, and passLowerCase all contain a confirm.
-// --If the user selects OK, they are TRUE.
-// --If the user selects CANCEL, they are FALSE.
+    console.log("yahello");
 
-
-// PASSWORD LENGTH 
-
-var passLength = function() {
-
-var passLengthPrompt = window.prompt("How many characters?");
-
-    if (passLengthPrompt >= 8 && passLengthPrompt <= 128) {
-
-      console.log("Damn, this works?");
-    
     } else {
-    
-    passLengthPrompt = window.alert("Your password must contain 8 or more and no less than 128 characters.");
-    return passLength();
-      
-    }
 
-  };
+    window.alert("Your password must be between 8 and 126 characters long.")
+    return generatePassword();
 
-// SPECIAL CHARACTERS
-
-var passSpCharacter = function() {
-
-  passSpCharacterConfirm = window.confirm("Would you like your password to have special characters?");
+  }
 
   if (passSpCharacterConfirm) {
 
+    passSpCharacterConfirm = true;
+
     console.log("hello?");
 
-  } else {
+    } else {
 
     console.log("goodbye");
 
-  }
-
-};
-
-// UPPER CASE
-
-var passUpperCase = function() {
-
-  var passUpperCaseConfirm = window.confirm("Would you like your password to have upper case?");
+    }
 
   if (passUpperCaseConfirm) {
 
+    passUpperCaseConfirm = true;
+
     console.log("upper case worked");
 
-  } else {
+    } else {
 
     console.log("upper case did not work")
 
-  }
-}
-
-// LOWER CASE
-
-var passLowerCase = function() {
-
-  var passLowerCaseConfirm = window.confirm("would you like your password to have lower case?");
+    }
 
   if (passLowerCaseConfirm) {
 
+    passLowerCaseConfirm = true;
+
     console.log("lower case worked");
 
-  } else {
+    } else {
 
     console.log("lower case did not work")
 
-  }
-}
+    }
 
-//END OF PROMPT FUNCTIONS
+  if (passSpCharacterConfirm === false && passUpperCaseConfirm === false && passLowerCaseConfirm === false) {
+    window.alert("A strong password needs at least one character type! Please select one.");
+    return generatePassword();
+  }
+
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
